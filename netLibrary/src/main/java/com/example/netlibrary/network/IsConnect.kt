ff7017b1c3d227213a26078d2316d887f1b3769a
frens <liadovn@yandex.ru>
2026-07-29 23:56:21 +0300

    create Network monitor

---
+++ netLibrary/src/main/java/com/example/netlibrary/network/IsConnect.kt
@@ -0,0 +1,5 @@
+package com.example.netlibrary.network
+
+interface IsConnect {
+    fun IsConnected(): Boolean
+}
---
+++ netLibrary/src/main/java/com/example/netlibrary/network/NetworkMonitor.kt
@@ -0,0 +1,14 @@
+package com.example.netlibrary.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+class NetworkMonitor(private val context: Context): IsConnect {
+    override fun IsConnected(): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork ?:return false
+        val capabilities = connectivityManager.getNetworkCapabilities(network)?: return false
+        return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+    }
+}
